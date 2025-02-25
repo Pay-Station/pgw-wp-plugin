0a2254caadfc48218150f9dfa315727055751872
Shahadat Hossain <shahadatsdk@gmail.com>
2025-02-25 12:24:54 +0600

    introduce new Payment API

--- payment.php
+++ payment.php
@@ -9,70 +9,52 @@ if (isset($_POST["access"])) {
     $cust_email = $_POST["billing_email"];
     $cust_address = $_POST["billing_address_1"];
     $invoice_number = $_POST["invoice_number"];
-    $pay_with_charge = isset($_POST["charge_for_customer"]) ? $_POST["charge_for_customer"] : 0; // if charge for customer == 1 then charge will add with invoice else merchant will bear
+    $pay_with_charge = isset($_POST["charge_for_customer"]) ? $_POST["charge_for_customer"] : 0;
     $baseurl = $_POST["baseurl"];
     $returnURL = $_POST["returnURL"];
+	$merchantId = $data["merchantId"];
+	$password = $data["password"];
 
-    $header = array(
-        'storeId:' . $data["merchantId"],
-        'storePassword:' . $data["password"]
-    );
+	$post_feild = array(
+		'invoice_number' => 'WP000'.$merchantId.$invoice_number,
+		'currency' => "BDT",
+		'payment_amount' => $cartTotal,
+		'cust_name' => $cust_name,
+		'cust_phone' => $cust_phone,
+		'cust_email' => $cust_email,
+		'cust_address' => $cust_address,
+		'reference' => "WP-Website",
+		'callback_url' => $returnURL,
+		'checkout_items' => "checkout_items",
+		'pay_with_charge' => $pay_with_charge,
+		'merchantId' => $merchantId,
+		'password' => $password
+	);
+	
+	$url = "https://api.paystation.com.bd/initiate-payment"; // API endpoint URL
+	$ch = curl_init(); // Initialize cURL session
 
-    $url = curl_init("https://api.paystation.com.bd/grant-token");
-    curl_setopt($url, CURLOPT_HTTPHEADER, $header);
-    curl_setopt($url, CURLOPT_CUSTOMREQUEST, "POST");
-    curl_setopt($url, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($url, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($url, CURLOPT_FOLLOWLOCATION, 1);
-    $tokenData = curl_exec($url);
-    curl_close($url);
-    $tokenArray = json_decode($tokenData, true);
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $post_feild);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 
-    if (array_key_exists("status", $tokenArray) && $tokenArray["status"] == "success") {
-        $token = $tokenArray["token"];
-        $header = array(
-            'token:' . $token
-        );
-        $post_feild = array(
-            'invoice_number' => '000'.$invoice_number,
-            'currency' => "BDT",
-            'payment_amount' => $cartTotal,
-            'cust_name' => $cust_name,
-            'cust_phone' => $cust_phone,
-            'cust_email' => $cust_email,
-            'cust_address' => $cust_address,
-            'reference' => "Website",
-            'callback_url' => $returnURL,
-            'checkout_items' => "checkout_items",
-            'pay_with_charge' => $pay_with_charge
-        );
-
-        $url = curl_init("https://api.paystation.com.bd/create-payment");
-        curl_setopt($url, CURLOPT_HTTPHEADER, $header);
-        curl_setopt($url, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($url, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($url, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($url, CURLOPT_POSTFIELDS, $post_feild);
-        curl_setopt($url, CURLOPT_FOLLOWLOCATION, 1);
-        $responseData = curl_exec($url);
-        curl_close($url);
-        $responseArray = json_decode($responseData, true);
-        if (array_key_exists("status", $responseArray) && $responseArray["status"] == "success") {
-            $rtData["status"] = "success";
-            $rtData["statusCode"] = $responseArray["status_code"];
-            $rtData["message"] = $responseArray["message"];
-            $rtData["payment_url"] = $responseArray["payment_url"];
-            echo json_encode($rtData);
-        } else {
-            $rtData["status"] = "failed";
-            // $rtData["statusCode"] = $responseArray["statusCode"];
-            $rtData["message"] = $responseArray["message"];
-            echo json_encode($rtData);
-        }
-    } else {
-        $rtData["status"] = "failed";
-        $rtData["statusCode"] = $tokenArray["statusCode"];
-        $rtData["message"] = $tokenArray["message"];
-        echo json_encode($rtData);
-    }
+	$responseData = curl_exec($ch);
+	curl_close($ch);
+	
+	$responseArray = json_decode($responseData, true);
+	if (array_key_exists("status", $responseArray) && $responseArray["status"] == "success") {
+		$rtData["status"] = "success";
+		$rtData["statusCode"] = $responseArray["status_code"];
+		$rtData["message"] = $responseArray["message"];
+		$rtData["payment_url"] = $responseArray["payment_url"];
+		echo json_encode($rtData);
+	} else {
+		$rtData["status"] = "failed";
+		// $rtData["statusCode"] = $responseArray["statusCode"];
+		$rtData["message"] = $responseArray["message"];
+		echo json_encode($rtData);
+	}
 }
+
